81401f5c7522058dc5c580b0a22b33655f7d0a28
enriquegomeztagle <0241823@up.edu.mx>
2025-01-15 15:30:49 -0600

    Using amazon bedrock with boto3 sdk in Mojo

--- boto3-tests/run.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-# Step 1: Run the Lua script to install requirements
-subprocess.run(["lua", "install_requirements.lua"], check=True)
-
-# Step 2: Run the Mojo code
-subprocess.run(["mojo", "run", "main.mojo"], check=True)
